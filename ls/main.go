c28d57708e653add9dc4939c6959490589a39690
ikeohachidi <ikeohachidi@gmail.com>
2022-07-10 17:48:28 +0100

    feat: hide hidden values by default and display with right flag

--- ls/main.go
+++ ls/main.go
@@ -1,34 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 )
 
 func main() {
-	path := "."
-	args := os.Args
 	printOut := ""
 
-	if len(args) > 1 {
-		path = args[1]
-	}
+	allFlag := flag.Bool("a", false, "Display all contents of folder including hidden items")
+	pathFlag := flag.String("p", ".", "")
+	flag.Parse()
 
-	dirs, err := os.ReadDir(path)
+	dirs, err := os.ReadDir(*pathFlag)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, dir := range dirs {
-		if dir.IsDir() {
-			printOut += fmt.Sprintf("%s ", color.CyanString(dir.Name()))
+		if strings.HasPrefix(dir.Name(), ".") {
+			if *allFlag {
+				printer(&printOut, dir)
+			}
 		} else {
-			printOut += fmt.Sprintf("%s  ", dir.Name())
+			printer(&printOut, dir)
 		}
 	}
 
 	fmt.Println(printOut)
+}
+
+func printer(to *string, dir fs.DirEntry) {
+	if dir.IsDir() {
+		*to += fmt.Sprintf("%s ", color.CyanString(dir.Name()))
+	} else {
+		*to += fmt.Sprintf("%s  ", dir.Name())
+	}
 }
