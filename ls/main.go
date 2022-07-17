0ea39e4fe91ab08b3f4f49453b25ae4e06a4d399
ikeohachidi <ikeohachidi@gmail.com>
2022-07-17 12:30:05 +0100

    refactor: use strings.Builder for string manipulation

--- ls/main.go
+++ ls/main.go
@@ -6,15 +6,20 @@ import (
 	"io/fs"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/fatih/color"
 )
 
-func main() {
-	printOut := ""
+type CmdFlags struct {
+	all  bool
+	list bool
+}
 
+func main() {
 	allFlag := flag.Bool("a", false, "Display all contents of folder including hidden items")
+	listFlag := flag.Bool("l", false, "Display all information i can possibly code about the directory")
 	pathFlag := flag.String("p", ".", "")
 	flag.Parse()
 
@@ -23,23 +28,56 @@ func main() {
 		log.Fatal(err)
 	}
 
+	flags := CmdFlags{*allFlag, *listFlag}
+
+	var printOut strings.Builder
+
 	for _, dir := range dirs {
 		if strings.HasPrefix(dir.Name(), ".") {
 			if *allFlag {
-				printer(&printOut, dir)
+				printOut.WriteString(strBuilder(flags, dir))
 			}
 		} else {
-			printer(&printOut, dir)
+			printOut.WriteString(strBuilder(flags, dir))
 		}
 	}
 
-	fmt.Println(printOut)
+	fmt.Println(printOut.String())
 }
 
-func printer(to *string, dir fs.DirEntry) {
-	if dir.IsDir() {
-		*to += fmt.Sprintf("%s ", color.CyanString(dir.Name()))
-	} else {
-		*to += fmt.Sprintf("%s  ", dir.Name())
+func fileSize(dir fs.DirEntry) string {
+	info, err := dir.Info()
+	if err != nil {
+		return "nil"
 	}
+
+	perm := info.Mode().Perm().String()
+	size := strconv.Itoa(int(info.Size()/1000)) + "KB"
+
+	var str strings.Builder
+	str.WriteString(perm)
+	str.WriteByte('\t')
+	str.WriteString(size)
+
+	return str.String()
+}
+
+func strBuilder(flags CmdFlags, dir fs.DirEntry) string {
+	var str strings.Builder
+
+	if flags.list {
+		str.WriteString(fileSize(dir) + "\t")
+	}
+
+	if dir.IsDir() {
+		str.WriteString(color.CyanString(dir.Name()) + "\t")
+	} else {
+		str.WriteString(dir.Name() + "\t")
+	}
+
+	if flags.list {
+		str.WriteString("\n")
+	}
+
+	return str.String()
 }
