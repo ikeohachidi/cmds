82d85147526dfe3a51a2e8296508aac9f1261622
ikeohachidi <ikeohachidi@gmail.com>
2022-07-31 01:06:30 +0100

    feat: update options for mkdir command

--- mkdir/main.go
+++ mkdir/main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"strings"
 )
 
 func main() {
-	modeFlag := flag.Int("m", int(777), "Mode to create directory in")
+	modeFlag := flag.Int("m", 0777, "Mode to create directory in")
 	nameFlag := flag.String("n", "", "Name of folder")
+	pathFlag := flag.Bool("p", false, "Will create '/' separate paths")
+	verboseFlag := flag.Bool("v", false, "Be verbose")
 	flag.Parse()
 
 	if *nameFlag == "" {
@@ -17,5 +20,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	os.Mkdir(*nameFlag, fs.FileMode(*modeFlag))
+	// obviously the short and easy way
+	// os.MkdirAll(*nameFlag, fs.FileMode(*modeFlag))
+
+	folderNames := strings.Split(*nameFlag, "/")
+	var path string
+	if *pathFlag {
+		for _, folderName := range folderNames {
+			path += folderName + "/"
+			createFolder(path, fs.FileMode(*modeFlag), *verboseFlag)
+		}
+		return
+	}
+
+	if len(folderNames) > 0 {
+		createFolder(folderNames[0], fs.FileMode(*modeFlag), *verboseFlag)
+	}
+}
+
+func createFolder(path string, perm os.FileMode, showLog bool) {
+	if showLog {
+		fmt.Printf("Currently creating %v folder\n", path)
+	}
+	err := os.Mkdir(path, perm)
+	if err != nil {
+		fmt.Print(err)
+	}
 }
